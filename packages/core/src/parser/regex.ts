47c4e2c4183629694b8d651ab7b37583607a602e
mindgam3s <34487011+mindgam3s@users.noreply.github.com>
2026-01-03 15:19:28 +0100

    Rename regex key for HDR10Plus
    Rename regex key for HDR10Plus

--- packages/core/src/parser/regex.ts
+++ packages/core/src/parser/regex.ts
@@ -93,7 +93,7 @@ export const PARSE_REGEX: PARSE_REGEX = {
     'H-SBS': createRegex('h?(alf)?[ .\\-_]?(sbs|side[ .\\-_]?by[ .\\-_]?side)'),
     'DV+HDR': createRegex('dv[\\+]?hdr'),
     'DV+HDR10': createRegex('dv[\\+]?hdr[ .\\-_]?10(?![ .\\-_]?(?:\\+|p(lus)?))'),
-    'DV+HDR10+': createRegex('dv[\\+]?hdr[ .\\-_]?10[ .\\-_]?(p(lus)?|[+])'),
+    'DV+HDR10Plus': createRegex('dv[\\+]?hdr[ .\\-_]?10[ .\\-_]?(p(lus)?|[+])'),
   },
   audioTags: {
     Atmos: createRegex('atmos|ddpa\\d?'),
