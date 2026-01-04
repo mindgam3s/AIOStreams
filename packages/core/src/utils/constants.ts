cb7a1358c8688fded291a1ca928adbc135c0b91b
mindgam3s <34487011+mindgam3s@users.noreply.github.com>
2026-01-04 19:23:48 +0100

    Remove deprecated visual tags from VISUAL_TAGS
    Removed deprecated visual tags from the constants.

--- packages/core/src/utils/constants.ts
+++ packages/core/src/utils/constants.ts
@@ -840,9 +840,6 @@ export type FakeVisualTag = (typeof FAKE_VISUAL_TAGS)[number];
 
 const VISUAL_TAGS = [
   ...FAKE_VISUAL_TAGS,
-  'DV+HDR',
-  'DV+HDR10',
-  'DV+HDR10Plus',
   'HDR10+',
   'HDR10',
   'DV',
