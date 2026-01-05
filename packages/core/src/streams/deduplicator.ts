422c19ec9f453a54ce244d596093d144ba26c73f
mindgam3s <34487011+mindgam3s@users.noreply.github.com>
2026-01-05 12:03:38 +0100

    Revert "Update filename normalization regex to allow periods '.'"
    This reverts commit 664c7683cb071431db169c0f0668e30bcf5eb896.

--- packages/core/src/streams/deduplicator.ts
+++ packages/core/src/streams/deduplicator.ts
@@ -67,7 +67,7 @@ class StreamDeduplicator {
             /(mkv|mp4|avi|mov|wmv|flv|webm|m4v|mpg|mpeg|3gp|3g2|m2ts|ts|vob|ogv|ogm|divx|xvid|rm|rmvb|asf|mxf|mka|mks|mk3d|webm|f4v|f4p|f4a|f4b)$/i,
             ''
           )
-          .replace(/[^\p{L}\p{N}+.]/gu, '')
+          .replace(/[^\p{L}\p{N}+]/gu, '')
           .replace(/\s+/g, '')
           .toLowerCase();
         currentStreamKeyStrings.push(`filename:${normalisedFilename}`);
